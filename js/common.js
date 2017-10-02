9f7c2e16480090d7f41913ba9ff40ea7562936d4
ychaika <ychaika@corevalue.net>
2017-10-02 10:24:09 +0300

    finish

--- js/common.js
+++ js/common.js
@@ -5,103 +5,135 @@ document.addEventListener("DOMContentLoaded", function(e) {
     var timeField = document.getElementsByClassName("js-timeField")[0],
         seconds = document.getElementsByClassName("js-seconds")[0],
         minutes = document.getElementsByClassName("js-minutes")[0],
-        stopBtn = document.getElementsByClassName("js-stop")[0];
+        startBtn = document.getElementsByClassName("js-startBtn")[0],
+        stepBtn = document.getElementsByClassName("js-stepBtn")[0],
+        stopBtn = document.getElementsByClassName("js-stopBtn")[0],
+        stepList = document.getElementsByClassName("js-stepList")[0],
+        stepItem = document.getElementsByClassName("js-stepItem"),
+        secondsStep = 0,
+        minutesStep = 0,
+        timeStatus = "disable",
+        goTimer;
 
-    var goTimer;
+    var stepsArr = {};
+
+    if(localStorage["timeSteps"]) {
+        stepsArr =  JSON.parse(localStorage["timeSteps"]);
+    }   
+    
 
     /* start timer */
 
-    function startTimer (point) {
-        if(timeField.classList.contains("active")) {
-            return false
+    function startTimer () {
+        var startTime = minutesStep * 60 + secondsStep;
+
+        timeStatus = "active";
+        if(stopBtn.classList.contains("js-reset")) {
+            stopBtn.classList.add("js-stop");
+            stopBtn.classList.remove("red", "js-reset");
+            stopBtn.innerHTML = "Stop";
         }
-        if(document.getElementsByClassName("js-reset")[0]) {
-            document.getElementsByClassName("js-reset")[0].classList.add("js-stop");
-            document.getElementsByClassName("js-reset")[0].classList.remove("red", "js-reset");
-            document.getElementsByClassName("js-stop")[0].innerHTML = "Stop";
-        }
-        timeField.classList.add("active");
-        var startTime = point;
         goTimer = setInterval(function(){
             startTime++;
+            secondsStep = startTime % 60;
+            minutesStep = (startTime - secondsStep) / 60;
 
-            if(startTime < 10) {
-                seconds.innerHTML = "0" + startTime;
-            } else if(startTime > 59) {
-                seconds.innerHTML
-                var secondsPart = startTime % 60;
-                if (secondsPart < 10) {
-                    seconds.innerHTML = "0" + secondsPart;
-                } else {
-                    seconds.innerHTML = secondsPart;
-                }
-                var fullMinutes = (startTime - seconds.innerHTML) / 60;
-                if(fullMinutes < 10) {
-                    minutes.innerHTML = "0" + fullMinutes;
-                } else {
-                    minutes.innerHTML = fullMinutes;
-                }
+            if(secondsStep < 10) {
+                seconds.innerHTML = "0" + secondsStep;
             } else {
-                seconds.innerHTML =  startTime;
+                seconds.innerHTML = secondsStep;
             }
+
+            if(minutesStep < 10) {
+                minutes.innerHTML = "0" + minutesStep;
+            } else {
+                minutes.innerHTML = minutesStep;
+            }
+
         }, 1000);
-
     }
 
-    /* make step */ 
+    function writeLS() {
+        localStorage.setItem("timeSteps", JSON.stringify(stepsArr));
+    }
+    
+    /* display steps */
 
-    function makeStep() {
-        var step = document.createElement("div");
-        step.className = "step";
-        step.textContent = minutes.innerHTML + ":" + seconds.innerHTML;
-        document.getElementsByClassName("js-stepList")[0].appendChild(step);
+    function displaySteps() {
+        for(var key in stepsArr) {
+            var step = document.createElement("div");
+            step.className = "step js-stepItem ";
+            step.textContent = stepsArr[key];
+            step.setAttribute("data-key", key);
+            stepList.appendChild(step);
+        }
     }
 
-    /* stop timer */ 
-
-    function stopTimer () {
-        timeField.classList.remove("active");
-        clearInterval(goTimer);
-        stopBtn.innerHTML = "Reset";
-        stopBtn.classList.remove("js-stop");
-        setTimeout(function() {
-            stopBtn.classList.add("red", "js-reset");
-        }, 10);
-    }
+    displaySteps();
 
     /* events */ 
 
-    document.getElementsByClassName("js-start")[0].addEventListener("click", function(e){
+    startBtn.addEventListener("click", function(e){
         e.preventDefault();
-        var point = parseInt(minutes.innerHTML) * 60 + parseInt(seconds.innerHTML);
-        startTimer(point);
+        startTimer();
     });
 
-    document.getElementsByClassName("js-step")[0].addEventListener("click", function(e) {
+    stepBtn.addEventListener("click", function(e) {
+        var addSec,
+            addMin,
+            newKey; 
+
         e.preventDefault();
-        makeStep();
+        if(minutesStep < 10) {
+            addMin = "0" + minutesStep;
+        } else {
+            addMin = minutesStep;
+        }
+        if(secondsStep < 10) {
+            addSec = "0" + secondsStep;
+        } else {
+            addSec = secondsStep;
+        }
+
+        if(Object.keys(stepsArr).length == 0) {
+            newKey = 0;
+        } else {
+            var arrKeys = Object.keys(stepsArr);
+            newKey = parseInt(arrKeys[arrKeys.length - 1]) + 1;
+        }
+
+        stepsArr[newKey] = addMin + ":" + addSec;
+        stepList.innerHTML = "";
+
+        writeLS();
+        displaySteps();
     })
 
-    // stopBtn.addEventListener("click", function(e) {
-    //     e.preventDefault();
-    //     stopTimer();
-    // })
-
-    document.addEventListener("click", function(e) {
-        if(e.target.classList.contains("js-stop")) {
+    stopBtn.addEventListener("click", function() {
+        if(this.classList.contains("js-stop")) {
             e.preventDefault();
-            stopTimer();
+            timeStatus = "disable"
+            clearInterval(goTimer);
+            this.classList.remove("js-stop");
+            this.classList.add("red", "js-reset");
+            this.innerHTML = "Reset";
+        } else if (this.classList.contains("js-reset")) {
+            e.preventDefault();
+            minutes.innerHTML = "00";
+            seconds.innerHTML = "00";
+            minutesStep, secondsStep = 0;
+            this.classList.add("js-stop");
+            this.classList.remove("red", "js-reset");
+            this.innerHTML = "Stop";
         }
     })
 
     document.addEventListener("click", function(e) {
-        if(e.target.classList.contains("js-reset")) {
-            e.preventDefault();
-            minutes.innerHTML = "00";
-            seconds.innerHTML = "00";
-            e.target.classList.add("js-stop");
-            e.target.classList.remove("red", "js-reset");
-            e.target.innerHTML = "Stop";
+        if(e.target.classList.contains("js-stepItem")) {
+            var itemKey = e.target.getAttribute("data-key");
+            delete stepsArr[itemKey];
+            e.target.parentNode.removeChild(e.target);
+            writeLS();
         }
     })
 
